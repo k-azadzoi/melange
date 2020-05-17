3c2a479a347816b4ea2f05afcc97dc1e269cd21a
Ken Charette <charettekenneth@gmail.com>
2020-05-16 21:42:59 -0400

    Updates

---
+++ precache-manifest.dc59918b4f3ba45b55972aa5eaca1f85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f55645a8c828d790def24cdb105e814b",
+    "url": "/melange/index.html"
+  },
+  {
+    "revision": "bb609f5e71b7b2c28f3d",
+    "url": "/melange/static/css/main.a7da8eeb.chunk.css"
+  },
+  {
+    "revision": "bbee63f521c732f79e37",
+    "url": "/melange/static/js/2.de7d7bcc.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/melange/static/js/2.de7d7bcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb609f5e71b7b2c28f3d",
+    "url": "/melange/static/js/main.0067cfb7.chunk.js"
+  },
+  {
+    "revision": "4793174a97dc230628f6",
+    "url": "/melange/static/js/runtime-main.4a4a988b.js"
+  }
+]);
